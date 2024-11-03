81340489e7797560f5d5d2476a4e2dff40996252
lexSoured <lexsoured@gmail.com>
2024-11-03 10:49:05 +0300

    Изменены стили графического интерфейса. Добавлена функциональность в виде вывода информации о капитализвции валюты за 24 часа. Readme файл дополнен

--- main.py
+++ main.py
@@ -25,8 +25,9 @@ def generate_currency_lists():
         response.raise_for_status()  # Проверка на успешность запроса
         data = response.json()  # Преобразование ответа в JSON-формат
         for coin in data:
-            crypto_list.append(coin['name'])  # Извлечение ID криптовалюты
-            # Сохранение имени криптовалюты в словаре
+            # Добавление названия криптовалюты в список
+            crypto_list.append(coin['name'])
+            # Сохранение имени криптовалюты в словаре для дальнейшего использования
             currency_dict[coin['name']] = coin['id']
     except requests.RequestException as e:
         messagebox.showerror(
@@ -42,13 +43,13 @@ def get_exchange_rate(event=None):
         messagebox.showwarning('Предупреждение', 'Неверное название валюты')
         return
     base_code = currency_dict.get(base_name)
-    # Обновление метки с полным названием криптовалюты
+    # Обновление меток с полными названиями выбранных валют
     if base_code:
-        label_name.config(text=f'Выбранная криптовалюта:\n\t {base_name}')
-        label_name2.config(text=f'Выбранная фиатная валюта:\n\t {
+        label_name.config(text=f'Выбранная криптовалюта:\n {base_name}')
+        label_name2.config(text=f'Выбранная фиатная валюта:\n {
                            target_code.upper()}')
 
-        update_coin_image(base_code)
+        update_coin_info(base_code)
         update_flag(target_code)
     else:
         label_name.config(text='Криптовалюта не найдена')
@@ -67,41 +68,51 @@ def get_exchange_rate(event=None):
             # Извлечение курса обмена
             exchange_rate = data[base_code][target_code]
             label_entry.config(text=f'Курс обмена {base_code.upper()} к {
-                               target_code.upper()}: {exchange_rate}')
+                               # 4bcc00')
+                               target_code.upper()}: {exchange_rate}', foreground='#4bcc00')
         else:
-            label_entry.config(text='Отсутствуют данные торгов')
+            label_entry.config(
+                text='Отсутствуют данные торгов', foreground='#ff3a33')
     except requests.RequestException as e:
         messagebox.showerror('Ошибка', f'{e}')
     except KeyError:
         messagebox.showerror('Ошибка', 'Эта криптовалюта не торгуется')
 
 
-def update_coin_image(coin_id):
-    '''Функция для обновления изображения криптовалюты'''
+def update_coin_info(coin_id):
+    '''Функция для обновления данных о валюте'''
     url = f'https://api.coingecko.com/api/v3/coins/{coin_id}'
-    headers = {'accept': 'application/json'}  # Заголовки для запроса
+    headers = {'accept': 'application/json'}
     try:
-        # Выполнение HTTP-запроса
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Проверка на успешность запроса
-        data = response.json()  # Преобразование ответа в JSON-формат
-        # Получение ссылки на маленькое изображение
+        response.raise_for_status()
+        data = response.json()
+
+        # Получение ссылки на маленькое изображение криптовалюты
         image_url = data.get('image', {}).get('small', '')
         if image_url:
             try:
                 response = requests.get(image_url)
                 response.raise_for_status()
-                # Преобразование содержимого ответа в байты
                 img_data = BytesIO(response.content)
-                img = Image.open(img_data)  # Открытие изображения
+                img = Image.open(img_data)
                 img.thumbnail((50, 50), Image.Resampling.LANCZOS)
                 img_tk = ImageTk.PhotoImage(img)
                 label_image.config(image=img_tk)
-                # Сохранение ссылки на изображение, чтобы оно не было удалено сборщиком мусора
-                label_image.image = img_tk
+                label_image.image = img_tk  # Сохранение ссылки на изображение
             except requests.RequestException as e:
                 messagebox.showerror(
                     'Ошибка', f'Ошибка при загрузке изображения: {e}')
+
+        # Получение изменения рыночной капитализации
+        market_cap_change_percentage_24h = data['market_data']['market_cap_change_percentage_24h']
+        if market_cap_change_percentage_24h is not None:
+            if market_cap_change_percentage_24h > 0:
+                color = 'green'
+            else:
+                color = 'red'
+            label_market_cap_change.config(text=f'Изменение рыночной капитализации за 24 часа: {
+                                           market_cap_change_percentage_24h:.2f}%', foreground=color)
     except requests.RequestException as e:
         messagebox.showerror(
             'Ошибка', f'Ошибка при получении данных криптовалюты: {e}')
@@ -133,16 +144,16 @@ def filter_currencies(event, combobox, crypto_list):
 
 # Создание главного окна приложения
 root = tk.Tk()
+
 root.title('Курсы обмена криптовалют')  # Установка заголовка окна
-root.geometry(f'600x500+{root.winfo_screenwidth()//2 -
-              300}+{root.winfo_screenheight()//2-250}')  # Установка размеров окна
-# Установка прозрачности и всегда поверх других окон
-root.attributes('-alpha', 0.9, '-topmost', True)
-root.config(bg='#1aa698', borderwidth=12, border=5, padx=20,
+# Установка размеров и позиции окна
+root.geometry(f'600x500+{root.winfo_screenwidth()//
+              2 - 300}+{root.winfo_screenheight()//2-250}')
+root.config(bg='#0d1217', borderwidth=12, padx=20,
             pady=20)  # Настройка фона и границ окна
 # Генерация списков названий криптовалют и фиатных валют
 generate_currency_lists()
-# Настройка веса колонок и строк
+# Настройка веса колонок и строк для правильного расположения элементов
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_rowconfigure(0, weight=1)
@@ -153,20 +164,18 @@ root.grid_rowconfigure(4, weight=1)
 root.grid_rowconfigure(5, weight=1)
 
 style = ttk.Style()
+style.theme_use('alt')
 style.configure(
     'TLabel',  # Имя стиля
-    foreground='#fefe2e',  # Цвет текста
-    background='#1aa698',  # Цвет фона
+    foreground='#dfe5ec',  # Цвет текста
+    background='#0d1217',  # Цвет фона
     font=('Roboto', 12, 'bold'),  # Шрифт
-    padding=(10, 10),  # Отступы (горизонтальный, вертикальный)
+    padding=(10, 10)  # Отступы (горизонтальный, вертикальный)
 )
 
 # Создание метки для инструкции по выбору криптовалюты
 label_crypto = ttk.Label(
-    root,
-    style='TLabel',
-    text='Выберите криптовалюту\n (Можно воспользоваться\n вводом с клавиатуры):',
-)
+    root, style='TLabel', text='Выберите криптовалюту\n (Можно воспользоваться\n вводом с клавиатуры):')
 # Размещение метки в окне с отступами
 label_crypto.grid(column=0, row=0, sticky='nw')
 
@@ -176,11 +185,11 @@ label_fiat = ttk.Label(root, style='TLabel', text='Выберите фиатну
 label_fiat.grid(column=1, row=0, sticky='ne')
 
 # Создание метки для отображения полного названия криптовалюты
-label_name = ttk.Label(root, style='TLabel')
+label_name = ttk.Label(root, style='TLabel', justify='center')
 label_name.grid(column=0, row=2)  # Размещение метки в окне с отступами
 
 # Создание метки для отображения полного названия фиатной валюты
-label_name2 = ttk.Label(root, style='TLabel')
+label_name2 = ttk.Label(root, style='TLabel', justify='center')
 label_name2.grid(column=1, row=2)  # Размещение метки в окне с отступами
 
 # Создание метки для отображения изображения криптовалюты
@@ -191,55 +200,62 @@ label_image.grid(column=0, row=3)  # Размещение метки в окне
 label_image2 = ttk.Label(root, style='TLabel')
 label_image2.grid(column=1, row=3)  # Размещение метки в окне с отступами
 
+# Создание метки для отображения изменения рыночной капитализации
+label_market_cap_change = ttk.Label(root, style='TLabel')
+label_market_cap_change.grid(column=0, row=5, columnspan=2)
+
 # Создание метки для отображения результата
 label_entry = ttk.Label(root, style='TLabel')
 # Размещение метки в окне с отступами
 label_entry.grid(column=0, row=4, columnspan=2)
 
 style.configure(
-    'TCombobox',  # Имя стиля
-    foreground='#8626b5',  # Цвет текста
-    font=('Roboto', 16, 'bold'),  # Шрифт
-    padding=(10, 10),  # Отступы (горизонтальный, вертикальный)
-    arrowsize=12,  # Размер стрелки
+    'TCombobox',
+    fieldbackground='#e5e7eb',  # Цвет фона поля ввода
+    background='#35af00',       # Цвет фона кнопки (стрелки)
+    foreground='#0d1217',       # Цвет текста
+    padding=5,                  # Отступы внутри combobox
+    insertwidth=2               # Толщина курсора ввода
 )
 
 # Создание выпадающего списка для выбора криптовалюты
-combobox_crypto = ttk.Combobox(root, style='TCombobox', values=crypto_list)
+combobox_crypto = ttk.Combobox(root, style='TCombobox', values=crypto_list,
+                               width=30, justify='center', font=('Roboto', 12, 'bold'))
 # Размещение выпадающего списка в окне с отступами
 combobox_crypto.grid(column=0, row=1, pady=15, padx=10, sticky='nw')
 # Установка значения по умолчанию в выпадающем списке
 combobox_crypto.set('Idena')
 # Привязка функции к нажатию Enter
 combobox_crypto.bind('<Return>', get_exchange_rate)
+# Привязка функции фильтрации к событию отпускания клавиш
 combobox_crypto.bind('<KeyRelease>', lambda event: filter_currencies(
-    # Привязка функции фильтрации к событию отпускания клавиш
     event, combobox_crypto, crypto_list))
 
 # Создание выпадающего списка для выбора фиатной валюты
 combobox_fiat = ttk.Combobox(
-    root, style='TCombobox', values=fiat_list, state='readonly')
+    root, style='TCombobox', values=fiat_list, state='readonly', width=30, justify='center', font=('Roboto', 12, 'bold'))
 # Размещение выпадающего списка в окне с отступами
 combobox_fiat.grid(column=1, row=1, pady=15, padx=10, sticky='ne')
-combobox_fiat.set('USD')  # Установка значения по умолчанию в выпадающем списке
+# Установка значения по умолчанию в выпадающем списке
+combobox_fiat.set('USD')
 
 # Создание кнопки для получения курса обмена
 button = tk.Button(
     root,
     text='Получить курс обмена',
-    fg='#fefe2e',
-    bg='#1d655e',
-    activebackground='#00ce00',
-    activeforeground='#fff2e2',
-    font=('Arial', 12, 'bold'),
+    fg='#dfe5ec',
+    bg='#239200',
+    activebackground='#80e038',
+    activeforeground='#e5e7eb',
+    font=('Roboto', 12, 'bold'),
     padx=15,
     pady=10,
     relief='raised',
-    borderwidth=3,
+    borderwidth=5,
     command=get_exchange_rate,
 )
 # Размещение кнопки в окне с отступами
-button.grid(column=0, row=5, columnspan=2, sticky='s')
+button.grid(column=0, row=6, columnspan=2, sticky='s')
 
 # Запуск главного цикла приложения
 root.mainloop()
